96961f25dd4b91445e2e976ca8d4798f44db10e2
Happy <admin@maoyuan.us>
2024-08-27 14:38:15 +0800

    fix(bukkit): 防止dueyn插件集成中断言错误
    调整BukkitHuskTowns类以存储插件加载顺序，确保在依赖的插件（如dueyn）未加载时， HuskTowns不会尝试访问它们。此更改防止了由于插件依赖性错误排序而导致的断言

--- bukkit/src/main/java/net/william278/husktowns/BukkitHuskTowns.java
+++ bukkit/src/main/java/net/william278/husktowns/BukkitHuskTowns.java
@@ -115,6 +115,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.io.FileReader;
 import java.io.IOException;
 
