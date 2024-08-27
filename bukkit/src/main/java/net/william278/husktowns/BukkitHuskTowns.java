f7f329a62cabe355f32cacdfb7ae406a72d89bd1
Happy <admin@maoyuan.us>
2024-08-27 10:54:45 +0800

    fix

--- bukkit/src/main/java/net/william278/husktowns/BukkitHuskTowns.java
+++ bukkit/src/main/java/net/william278/husktowns/BukkitHuskTowns.java
@@ -65,6 +65,7 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.advancement.AdvancementProgress;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.entity.Player;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
